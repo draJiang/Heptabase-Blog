bd7f081407dcdf40f2732927ca9223c5cef1fbaa
draJiang <598353451@qq.com>
2022-11-06 17:08:36 +0800

    修复问题

--- src/constantFunction.js
+++ src/constantFunction.js
@@ -229,11 +229,8 @@ const getHeptabaseData = new Promise((resolve, reject) => {
     // 获取 Heptabase 数据
     fetch(
         'https://app.heptabase.com/api/whiteboard/?secret=d4cc3728297609add1a00aab108e90c4e57a1c378cfc2307c251745bf7d2a884',{
-            mode: 'no-cors',
-            method: "get",
-            headers: {
-                 "Content-Type": "application/json"
-            },
+            mode: 'cors',
+            method: "get"
         }
     )
         .then(res => res.json())
