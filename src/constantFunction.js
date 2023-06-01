a17e098b0233e6a237b41e57420210350099d3b9
draJiang <598353451@qq.com>
2023-06-02 00:08:07 +0800

    Fixed math_inline bug

--- src/constantFunction.js
+++ src/constantFunction.js
@@ -5,6 +5,23 @@ import { Button, Modal } from 'antd';
 
 const { confirm } = Modal;
 
+// import { db } from '@vercel/postgres';
+ 
+// export default async function handler(request, response) {
+//   const client = await db.connect();
+ 
+//   try {
+//     await client.sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
+//     const names = ['Fiona', 'Lucy'];
+//     await client.sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${names[1]});`;
+//   } catch (error) {
+//     return response.status(500).json({ error });
+//   }
+ 
+//   const pets = await client.sql`SELECT * FROM Pets;`;
+//   return response.status(200).json({ pets });
+// }
+
 const getCardName = (cardId) => {
     const whiteboardId = getWhiteboardIdFromUrl(window.location.href)
     const heptabase_blog_data = JSON.parse(localStorage.getItem(whiteboardId))
@@ -714,13 +731,25 @@ const heptaContentTomd = (content_list, parent_node, parent_card_id) => {
                 new_node.src = content_list[i]['attrs']['url']
                 break
 
+            case 'math_inline':
+                new_node = document.createElement('span')
+                break
+
             default:
                 break
 
         }
 
+
+
         if (new_node !== undefined && parent_node !== undefined) {
-            parent_node.appendChild(new_node)
+
+            try {
+                parent_node.appendChild(new_node)
+            } catch (error) {
+                console.log(parent_node);
+            }
+
         } else {
             console.log(parent_node);
         }
