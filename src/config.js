4887ef26b2a4f325698f9a92c589a7791b6e9ade
vincent <i@kii.la>
2024-01-10 20:21:07 +0800

    init repo.

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
-    'api_url': 'https://api.dabing.one/', // 数据请求地址
-    'title': 'Jiang 的数字花园🌱', // 站点标题
+    'api_url': 'https://example.com/', // 数据请求地址
+    'title': 'Kiila 的数字花园🌱', // 站点标题
     'pages': {
-        'Blogs': '9f31ea21-90b9-4523-b8d5-cb33b7a01bda',
-        'Projects': '3dd9a603-a7f3-44e9-a6d7-cd2ebda08952',
-        'Activity':'activity', // 花园活跃状态页面
-        'About': '3a433c0b-e2e1-4722-8a88-a17e9aa2b927'
+        'Blogs': 'XXXXXXXXX',
+        'Projects': 'XXXXXXXXX',
+        'Activity':'XXXXXXXXX', // 花园活跃状态页面
+        'About': 'XXXXXXXXX'
     }
 }
 
