1560d7b769424824742d8c0aa3eda4e768abb416
daoliu1221 <141800732+daoliu1221@users.noreply.github.com>
2024-03-04 22:08:31 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,11 +4,11 @@ const CONFIG = {
     'whiteboard_id': '09dba888a0c5beb7cadcb74ac8ffe96c52197df0de63eac2da86f6ea12c86fa5',
     'title': '稻流Daoliu', // 站点标题
     'pages': {
-        '文章': '87247526-363e-4ce0-9a62-49d6b5413650',
-        '作品': 'bc675d03-0479-4f07-999a-a8c4f60a166c',
-        '學習筆記': '8c4cf837-fb63-4f1d-b55d-d55c8e5bb4e4',
-        '書庫': '1296b49c-a636-464d-8201-98110437c95a',
-        '關於我': '6cca6d23-f99d-43b0-ae17-a55475571a3c'
+        'Articles': '87247526-363e-4ce0-9a62-49d6b5413650',
+        'Collection': 'bc675d03-0479-4f07-999a-a8c4f60a166c',
+        'Notes': '8c4cf837-fb63-4f1d-b55d-d55c8e5bb4e4',
+        'Library': '1296b49c-a636-464d-8201-98110437c95a',
+        'About': '6cca6d23-f99d-43b0-ae17-a55475571a3c'
     }
 }
 
