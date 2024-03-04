ed8101bd80cffc0b3132cc9207a8cc2ee4719566
daoliu1221 <141800732+daoliu1221@users.noreply.github.com>
2024-03-04 21:55:52 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,14 @@
 const CONFIG = {
     'api_url': '', // 废弃
     'ga': 'G-XXXXXX',
-    'whiteboard_id': '12e511518f5539c9ee5bc3c02fecab6d4f78a37eb8e13a2cfec484026b0d3c55',
-    'title': '数字花园🌱', // 站点标题
+    'whiteboard_id': '09dba888a0c5beb7cadcb74ac8ffe96c52197df0de63eac2da86f6ea12c86fa5',
+    'title': '稻流Daoliu', // 站点标题
     'pages': {
-        'Articles': '2e0bbcb8-fdf7-4cdb-8ee2-9f0651b71550',
-        'Projects': '2e0bbcb8-fdf7-4cdb-8ee2-9f0651b71550',
-        'Activity': 'activity', // 花园活跃状态页面
-        'About': '2e0bbcb8-fdf7-4cdb-8ee2-9f0651b71550'
+        '文章': '87247526-363e-4ce0-9a62-49d6b5413650',
+        '作品': 'bc675d03-0479-4f07-999a-a8c4f60a166c',
+        '學習筆記': '8c4cf837-fb63-4f1d-b55d-d55c8e5bb4e4',
+        '書庫': '1296b49c-a636-464d-8201-98110437c95a',
+        '關於我': '6cca6d23-f99d-43b0-ae17-a55475571a3c'
     }
 }
 
