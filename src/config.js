dd86c80d0db2e58f03afa8120a239fe932326945
draJiang <598353451@qq.com>
2024-03-07 23:25:45 +0800

    修复由于 CONFIG 配置导致的首页无法访问、导航栏无法点击的问题

--- src/config.js
+++ src/config.js
@@ -1,14 +1,28 @@
 const CONFIG = {
     'api_url': '', // 废弃
-    'ga': 'G-6C4Z9NHW8J',
-    'whiteboard_id': 'd4cc3728297609add1a00aab108e90c4e57a1c378cfc2307c251745bf7d2a884',
+    'ga': '',  // 废弃
+    'whiteboard_id': 'd4cc3728297609add1a00aab108e90c4e57a1c378cfc2307c251745bf7d2a884', // 公开的白板地址
     'title': 'Jiang 的数字花园🌱', // 站点标题
     'pages': {
-        'Articles': '9f31ea21-90b9-4523-b8d5-cb33b7a01bda',
+        'Articles': '9f31ea21-90b9-4523-b8d5-cb33b7a01bda', // 默认会将 About 作为主页，若不设置 About 则默认以这里设置的第 1 个卡片作为首页
         'Projects': '3dd9a603-a7f3-44e9-a6d7-cd2ebda08952',
         // 'Activity':'activity', // 花园活跃状态页面
-        'About': '3a433c0b-e2e1-4722-8a88-a17e9aa2b927'
+        'About': '3a433c0b-e2e1-4722-8a88-a17e9aa2b927',
     }
 }
+
+// // 测试数据
+// const CONFIG = {
+//     'api_url': '', // 废弃
+//     'ga': '',
+//     'whiteboard_id': '7caceb15dee4006d6a9cb1e084984ce1719db1f9079ee589de7cbf1ba34ba1c7',
+//     'title': 'Jiang 的数字花园🌱', // 站点标题
+//     'pages': {
+//         '文章': 'dcea580b-347f-4fef-82a1-23f4a553922a',
+//         '博客': '86bc9f0c-5ae7-47ef-88b4-02169872483b',
+//         // 'Activity':'activity', // 花园活跃状态页面
+//         '关于我/主页': '96b3d4fe-8632-4615-b9fc-d31cf3195ed5',
+//     }
+// }
 
 export default CONFIG
