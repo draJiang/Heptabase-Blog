a505f14d235e5de9b3732b54d1b2f51523dce171
makatadi <38482112+draJiang@users.noreply.github.com>
2024-03-11 00:43:49 +0800

    Removed node_modules

--- src/config.js
+++ src/config.js
@@ -4,10 +4,10 @@ const CONFIG = {
     'whiteboard_id': '12e511518f5539c9ee5bc3c02fecab6d4f78a37eb8e13a2cfec484026b0d3c55',
     'title': '数字花园🌱', // 站点标题
     'pages': {
-        'Articles': '2e0bbcb8-fdf7-4cdb-8ee2-9f0651b71550',
-        'Projects': '2e0bbcb8-fdf7-4cdb-8ee2-9f0651b71550',
-        'Activity': 'activity', // 花园活跃状态页面
-        'About': '2e0bbcb8-fdf7-4cdb-8ee2-9f0651b71550'
+        '简约至上': 'c9c148f0-af8d-40cb-a5bc-351e10ca70a2',
+        '动机': '4136d5fd-b0ec-4a81-9110-bca19716f884',
+        // '2': 'activity', // 花园活跃状态页面
+        '首页': '2e0bbcb8-fdf7-4cdb-8ee2-9f0651b71550'
     }
 }
 
