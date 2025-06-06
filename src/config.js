e03fa300c5b228ed92e84de56722a8b367cb3122
makatadi <38482112+draJiang@users.noreply.github.com>
2025-06-07 04:27:35 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ const CONFIG = {
     'title': '数字花园🌱', // 站点标题
     'pages': {
         'Articles': '9f31ea21-90b9-4523-b8d5-cb33b7a01bda',
+        'Letter':'fa7e9f94-8c81-494a-a54c-89c505035be9',
         'Projects': '3dd9a603-a7f3-44e9-a6d7-cd2ebda08952',
         // 'Activity':'activity', // 花园活跃状态页面
         'About': '3a433c0b-e2e1-4722-8a88-a17e9aa2b927'
